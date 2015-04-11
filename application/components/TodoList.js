f77831a057da0ff1e6651332a204f32e941f8ae0
Patte1808 <Patrick.Voss1808@gmail.com>
2015-04-11 21:13:37 +0200

    Fix visibility of todoitem text

--- application/components/TodoList.js
+++ application/components/TodoList.js
@@ -5,12 +5,13 @@ var TodoItem = require('./TodoItem');
 var {
   StyleSheet,
   ListView,
+  View,
 } = React;
 
 var TodoList = React.createClass({
 
   componentWillMount: function() {
-    var dataSource = new ListView.DataSource({
+    this.dataSource = new ListView.DataSource({
       rowHasChanged: (row1, row2) => row1 !== row2
     });
   },
@@ -19,16 +20,24 @@ var TodoList = React.createClass({
     var dataSource = this.dataSource.cloneWithRows(this.props.items);
 
     return (
-      <ListView
-        style = {styles.listView}
-        dataSource = {dataSource}
-        renderRow={(rowData) => <TodoItem data = {rowData} />}
-      />
+      <View style={styles.container}>
+        <ListView
+          style = {styles.listView}
+          dataSource = {dataSource}
+          renderRow={(rowData) => <TodoItem data = {rowData} />}
+        />
+      </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
   listView: {
     paddingTop: 20,
   }
